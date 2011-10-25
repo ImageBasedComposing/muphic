b9b3857b1fa92182760c9d35d3b2d9d93da6ce18
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-10-25 08:52:35 +0000

    + Ahora muphic toma los argumentos (mu toma el path de la imagen pero cambiando la extensión a .rec, por ahora)

--- trunk/Muphic/source/src/Muphic.cpp
+++ trunk/Muphic/source/src/Muphic.cpp
@@ -10,37 +10,51 @@ Muphic::~Muphic()
     //dtor
 }
 
+// works
+std::string changeExtension(std::string picPath, std::string extension)
+{
+	int i = picPath.find_last_of('.');
+
+	return picPath.substr(0, i) + '.' + extension;
+}
+
 int main( int argc, const char* argv[] )
 {
     /* Toma de parámetros de entrada */
+	
+	if (argc < 2)
+	{
+		cout << "Too few arguments in function call";
+		// showUsage();
+		return 1;
+	}
 
-    std::string usrConfPath = "";
-    std::string picPath = "";
+    std::string usrConfPath = argv[0];
+    std::string picPath = argv[1];
     std::string phicExe = "Phic.exe";
     std::string muExe = "Mu.exe";
 
     /* Ejecutar phic */
-
+	
     cout << "Comienza el analisis de la imagen" << endl;
 
     std::string picExeFile = phicExe + " " + usrConfPath + " " + picPath;
     system(picExeFile.c_str());
-    std::string analyzedPicPath = "";
-
+	
     cout << "Finaliza el analisis de la imagen" << endl;
     system("PAUSE");
 
-
     /* Ejecutar mu*/
-
+	
     cout << "Comienza la composicion" << endl;
+
+	std::string analyzedPicPath = changeExtension(picPath, "rec");
 
     std::string muExeFile = phicExe + " " + usrConfPath + " " + analyzedPicPath;
     system(muExeFile.c_str());
 
     cout << "Finaliza la composicion" << endl;
     system("PAUSE");
-
 
 	return 0;
 }
