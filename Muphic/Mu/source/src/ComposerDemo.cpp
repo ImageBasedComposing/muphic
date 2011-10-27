57586b4ec3421daaea7f53524aee9195892e8826
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-10-27 15:20:56 +0000

    + Se me olvidó subir composerDemo

---
+++ Muphic/Mu/source/src/ComposerDemo.cpp
@@ -0,0 +1,21 @@
+#include "ComposerDemo.h"
+
+
+ComposerDemo::ComposerDemo()
+{
+}
+
+ComposerDemo::~ComposerDemo()
+{
+}
+
+string ComposerDemo::compose()
+{
+	return "";
+}
+
+string ComposerDemo::compose(string picPath, string usrConfPath)
+{
+	return "";
+}
+
