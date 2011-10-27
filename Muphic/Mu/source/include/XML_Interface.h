13ac655959f680c40ce84ccb3551f88f11c30a17
vlad12354 <vlad12354@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-10-27 16:43:49 +0000

    + Empezada clase ComposerDemo + Añadido TinyXML + Añadido carpeta Resources con un xml del test1.png + Clases de TinyXML añadidas al proyecto Mu

--- Muphic/Mu/source/include/XML_Interface.h
+++ Muphic/Mu/source/include/XML_Interface.h
@@ -1,12 +1,12 @@
 #ifndef XML_INTERFACE_H
 #define XML_INTERFACE_H
 
-
 class XML_Interface
 {
     public:
         XML_Interface();
         virtual ~XML_Interface();
+
     protected:
     private:
 };
--- Muphic/Mu/source/src/ComposerDemo.cpp
+++ Muphic/Mu/source/src/ComposerDemo.cpp
@@ -11,11 +11,47 @@ ComposerDemo::~ComposerDemo()
 
 string ComposerDemo::compose()
 {
+
 	return "";
 }
 
 string ComposerDemo::compose(string picPath, string usrConfPath)
 {
-	return "";
+	/**
+		Parte de ABC
+	**/
+
+	string abcFile= "";
+	//Añadimos id X: 1 por defecto
+	abcFile += "X:1\n";
+	//Titulo
+	abcFile += "T:"+picPath+"\n";
+	//Tonalidad Do Mayor por defecto (C)
+	abcFile += "M:C\n";
+
+
+	/**
+		Parte de XML
+	**/
+	//Abrimos el archivo xml
+	TiXmlDocument doc((picPath+".xml").c_str());
+	doc.LoadFile();
+
+	//Siempre tiene que haber un declaración al principio (cabecera) <?xml version="1.0" ... ?>
+	TiXmlDeclaration* declDoc = (TiXmlDeclaration*)doc.FirstChild();
+	//Nos posicionamos en shapes:
+	TiXmlNode* shapesNode = doc.NextSibling();
+
+	//Nos recorremos las figuras:
+	TiXmlElement* figura;
+	TiXmlElement* color;
+	TiXmlElement* listaVertices;
+	for( figura = shapesNode->FirstChildElement(); figura; figura = shapesNode->NextSiblingElement() )
+	{
+		
+	}
+
+	return abcFile;
 }
+
 
