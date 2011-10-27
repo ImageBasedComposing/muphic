5ec3689889088a2792a6c4ebed9d88df38ebd2ad
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-10-27 14:47:42 +0000

    + Versión estable de muphic subida, ya funciona en su path y encuentra los exe y todo; con ejecutable actualizado

--- Muphic/source/src/Muphic.cpp
+++ Muphic/source/src/Muphic.cpp
@@ -18,44 +18,64 @@ std::string changeExtension(std::string picPath, std::string extension)
 	return picPath.substr(0, i) + '.' + extension;
 }
 
+std::string getPath(std::string exeFile)
+{
+	int i = exeFile.find_last_of("/\\");
+
+	if (i > 0)
+		return exeFile.substr(0, i);
+	else
+		return "";
+}
+
+void showUsage()
+{
+	cout << "Muphic.exe userConfPath imagePath" << endl;
+}
+
+
 int main( int argc, const char* argv[] )
 {
     /* Toma de parámetros de entrada */
-	
+
 	if (argc < 3)
 	{
 		cout << "Too few arguments in function call" << endl;
-		// showUsage();
+		showUsage();
 		system("PAUSE");
 		return 1;
 	}
 
-    std::string usrConfPath = argv[0];
-    std::string picPath = argv[1];
-    std::string phicExe = "Phic.exe";
-    std::string muExe = "Mu.exe";
+    std::string usrConfPath = argv[1];
+    std::string picPath = argv[2];
+
+	std::string executionPath = getPath(argv[0]);
+    std::string phicExe = executionPath + "\\Phic.exe";
+    std::string muExe = executionPath + "\\Mu.exe";
 
     /* Ejecutar phic */
-	
-    cout << "Comienza el analisis de la imagen" << endl;
+
+    cout << "Comienza el analisis de la imagen" << endl << endl;
 
     std::string picExeFile = phicExe + " " + usrConfPath + " " + picPath;
     system(picExeFile.c_str());
 	
-    cout << "Finaliza el analisis de la imagen" << endl;
-    system("PAUSE");
+    cout << endl << "Finaliza el analisis de la imagen" << endl;
+	cin.get();
+	cin.ignore(cin.rdbuf()->in_avail());
 
     /* Ejecutar mu*/
-	
-    cout << "Comienza la composicion" << endl;
+
+    cout << "Comienza la composicion" << endl << endl;
 
 	std::string analyzedPicPath = changeExtension(picPath, "rec");
 
     std::string muExeFile = muExe + " " + usrConfPath + " " + analyzedPicPath;
     system(muExeFile.c_str());
 
-    cout << "Finaliza la composicion" << endl;
-    system("PAUSE");
+    cout << endl << "Finaliza la composicion" << endl;
+	cin.get();
+	cin.ignore(cin.rdbuf()->in_avail());
 
 	return 0;
 }
