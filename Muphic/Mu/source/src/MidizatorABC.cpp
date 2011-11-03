d3a4f244053b1a801ec681203ecfaf35bcc5101a
strauzen <strauzen@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-11-03 16:34:16 +0000

    + Empezada la función de verdad de MidizatorABC.cpp

--- Muphic/Mu/source/src/MidizatorABC.cpp
+++ Muphic/Mu/source/src/MidizatorABC.cpp
@@ -13,7 +13,28 @@ MidizatorABC::~MidizatorABC()
 
 string MidizatorABC::toMidi(Music* music) 
 { 
-	return NULL; 
+	string fName = music->getName();
+	ofstream* f = new ofstream();
+	f->open(fName + ".abc");
+
+	//Escribimos en el fichero
+	*f << "X:1\n";
+	*f << "T:" + fName + '\n';
+	*f << "M:";
+	*f << music->getTempo().upper;
+	*f << '/';
+	*f << music->getTempo().lower;
+	*f << '\n';
+	//*f << "K:" + music->getClave();
+	
+	f->close();
+
+	string converter = "abc2midi.exe";
+
+	std::string picExeFile = converter + " " + fName + ".abc";
+    system(picExeFile.c_str());
+
+	return fName + ".mid"; 
 }
 
 string MidizatorABC::toMidi(std::string music, std::string converter)
