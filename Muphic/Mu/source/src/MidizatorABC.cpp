be7253eb441920f38d659e1bf21c77e256461df2
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-11-06 22:28:31 +0000

    + Realizada la compilación en linux del proyecto. Ahora el código tiene, a la hora de ejecutar archivos, 2 partes distintas, una para windows (se activa con la macro __WINDOWS) y otra para linux (__LINUX). He hecho que los proyectos de vs2010 tengan la macro __WINDOWS por defecto puesta, y los de codeblocks la de linux, para más comodidad de curro. Con esto ya tenemos las 2 versiones del proyecto pa nosotros pa siempre, porque más archivos no vamos a ejecutar, creo, así que esa parte ni la tocamos y chachi. He puesto también que el directorio de trabajo de los proyectos de codeblocks sea el bin/debug, que si no es un cristo. + reparada getPath (la de aux_functions.h, como se llame), para que ponga '/' en vez de '\' en los paths y que linux no llore. + añadido el exe de abc2midi para linux (abc2midi_linux.exe), aunque lo he compilado en mi casa así que a saber. + cambiado un include tinyxml, que estaba con mayúsculas y codeblocks gritaba.

--- Muphic/Mu/source/src/MidizatorABC.cpp
+++ Muphic/Mu/source/src/MidizatorABC.cpp
@@ -1,6 +1,13 @@
 #include "MidizatorABC.h"
 #include "aux_functions.h"
 
+#ifdef __LINUX
+    #include <unistd.h>
+    #include <sys/wait.h>
+#endif
+
+#include <process.h>
+
 MidizatorABC::MidizatorABC()
 {
     //ctor
@@ -11,41 +18,98 @@ MidizatorABC::~MidizatorABC()
     //dtor
 }
 
-string MidizatorABC::toMidi(Music* music) 
-{ 
+string MidizatorABC::toMidi(Music* music)
+{
 	string fName = music->getName();
+	char *const abcFile = const_cast<char*>((fName + ".abc").c_str());
+
 	ofstream* f = new ofstream();
-	f->open(fName + ".abc");
+	f->open(abcFile);
 
 	//Escribimos en el fichero
 	*f << "X:1\n";
-	*f << "T:" + fName + '\n';
+	*f << "T:" + fName << endl;
 	*f << "M:";
 	*f << music->getTempo().upper;
 	*f << '/';
 	*f << music->getTempo().lower;
-	*f << '\n';
+	*f << endl;
 	//*f << "K:" + music->getClave();
-	
+
 	f->close();
 
 	string converter = "abc2midi.exe";
 
-	std::string picExeFile = converter + " " + fName + ".abc";
-    system(picExeFile.c_str());
+	#ifdef __WINDOWS
 
-	return fName + ".mid"; 
+		std::string picExeFile = converter + " " + fName + ".abc";
+		system(picExeFile.c_str());
+
+	#endif
+
+
+    #ifdef __LINUX
+
+		converter = "abc2midi_linux.exe";
+
+		char *const params[] = {const_cast<char*>(converter.c_str()), abcFile, NULL};
+
+        pid_t pid;
+
+        if ((pid = fork()) ==-1)
+            cout << "fork error";
+         else if (pid == 0) {
+            execv(params[0], params);
+            cout << "Return not expected. Must be an execv error.";
+        }
+         else
+        {
+            int status;
+            while(wait(&status) > 0);
+        }
+
+    #endif
+
+    return fName + ".mid";
 }
 
 string MidizatorABC::toMidi(std::string music, std::string converter)
-{ 
+{
 	if (converter == "")
 		converter = "abc2midi.exe";
 
-	std::string picExeFile = converter + " " + music;
-    system(picExeFile.c_str());
 
-	std::string output = changeExtension(music, "mid");
+    #ifdef __WINDOWS
+
+		std::string picExeFile = converter + " " + music;
+		system(picExeFile.c_str());
+
+	#endif
+
+
+    #ifdef __LINUX
+
+        converter = "abc2midi_linux.exe";
+        char *const params[] = {const_cast<char*>(converter.c_str()), const_cast<char*>(music.c_str()), NULL};
+
+
+        pid_t pid;
+
+        if ((pid = fork()) ==-1)
+            cout << "fork error";
+         else if (pid == 0) {
+            execv(params[0], params);
+            cout << "Return not expected. Must be an execv error.";
+         }
+         else
+         {
+            int status;
+            while(wait(&status) > 0);
+         }
+
+     #endif
+
+    std::string output = changeExtension(music, "mid");
 
 	return output;
 }
