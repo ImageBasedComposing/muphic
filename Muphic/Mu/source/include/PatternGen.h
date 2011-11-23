e4c7ada7509e96a85647bdd317bec588bc9986ac
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-11-23 20:14:59 +0000

    + PatterGen ahora puede tragarse listas de segmentos también

--- Muphic/Mu/source/include/PatternGen.h
+++ Muphic/Mu/source/include/PatternGen.h
@@ -18,9 +18,14 @@ class PatternGen
         virtual ~PatternGen();
 
 		list< pair<int, float> > getPattern(list< pair<int, float> > partes);
+
+
 		list< pair<Segmento*, int> > getPattern(list< pair<Segmento*, int> > partes);
+		list< pair < list<Segmento*>* , int> > getPattern(list< pair < list<Segmento*>* , int> > partes);
 
     protected:
+		list< pair<void*, int> > getPatternV(list< pair <void* , int> > partes);
+
     private:
 };
 
--- Muphic/Mu/source/src/PatternGen.cpp
+++ Muphic/Mu/source/src/PatternGen.cpp
@@ -22,15 +22,57 @@ list< pair<int, float> > PatternGen::getPattern(list< pair<int, float> > partes)
 	return p->createPattern(partes);
 }
 
-		
 list< pair<Segmento*, int> > PatternGen::getPattern(list< pair<Segmento*, int> > partes)
 {
-	list< pair<int, float> > tmpin, tmpout;
+	list< pair<void*, int> > tmpin,tmpout;
 	list< pair<Segmento*, int> > out;
+
+	for (list< pair<Segmento*, int>  >::iterator it = partes.begin(); it != partes.end(); it++)
+	{
+		tmpin.push_back(make_pair((void*) (*it).first,(*it).second));
+	}
+
+	tmpout = getPatternV(tmpin);
+
+	for (list< pair<void*, int> >::iterator it = tmpout.begin(); it != tmpout.end(); it++)
+	{
+		out.push_back(make_pair((Segmento*) (*it).first,(*it).second));
+	}
+
+	return out;
+}
+
+list< pair < list<Segmento*>* , int> > PatternGen::getPattern(list< pair < list<Segmento*>* , int> > partes)
+{
+	list< pair<void*, int> > tmpin,tmpout;
+	list <pair < list<Segmento*>* , int> > out;
+
+	for (list <pair < list<Segmento*>* , int> >::iterator it = partes.begin(); it != partes.end(); it++)
+	{
+		tmpin.push_back(make_pair((void*) (*it).first,(*it).second));
+	}
+
+	tmpout = getPatternV(tmpin);
+
+	for (list< pair<void*, int> >::iterator it = tmpout.begin(); it != tmpout.end(); it++)
+	{
+		out.push_back(make_pair((list<Segmento*>*) (*it).first,(*it).second));
+	}
+
+	return out;
+}
+
+
+// MAGIC HERE
+
+list< pair<void*, int> > PatternGen::getPatternV(list< pair<void*, int> > partes)
+{
+	list< pair<int, float> > tmpin, tmpout;
+	list< pair<void*, int> > out;
 
 	int nsegmentos = 0;
 
-	for (list< pair<Segmento*, int> >::iterator it = partes.begin(); it != partes.end(); it++)
+	for (list< pair<void*, int> >::iterator it = partes.begin(); it != partes.end(); it++)
 	{
 		tmpin.push_back(make_pair((int) (*it).first,(*it).second));
 		nsegmentos += (*it).second;
@@ -50,7 +92,7 @@ list< pair<Segmento*, int> > PatternGen::getPattern(list< pair<Segmento*, int> >
 		else
 			tmpSeg = (*it).second;
 
-		out.push_back(make_pair((Segmento*) (*it).first,tmpSeg));
+		out.push_back(make_pair((void*) (*it).first,tmpSeg));
 		i++;
 	}
 
