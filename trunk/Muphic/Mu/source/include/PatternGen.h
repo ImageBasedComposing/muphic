82e0b9e358b7180b106ef36e9bc2764fe36f3cf8
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-11-23 20:35:54 +0000

    + PatterGen ahora es un template, funciona con cualquier tipo al que se le inicialice y se traga punteros a listas

--- trunk/Muphic/Mu/source/include/PatternGen.h
+++ trunk/Muphic/Mu/source/include/PatternGen.h
@@ -10,23 +10,75 @@
 
 using namespace std;
 
-
+template <class T>
 class PatternGen
 {
     public:
         PatternGen();
         virtual ~PatternGen();
 
+		list< pair< T, int> >* getPattern(list< pair<T, int> >* partes);
+
+    protected:		
+
 		list< pair<int, float> > getPattern(list< pair<int, float> > partes);
-
-
-		list< pair<Segmento*, int> > getPattern(list< pair<Segmento*, int> > partes);
-		list< pair < list<Segmento*>* , int> > getPattern(list< pair < list<Segmento*>* , int> > partes);
-
-    protected:
-		list< pair<void*, int> > getPatternV(list< pair <void* , int> > partes);
-
     private:
 };
+
+template <class T>
+PatternGen<T>::PatternGen()
+{
+}
+
+template <class T>
+PatternGen<T>::~PatternGen()
+{
+}
+
+template <class T>
+list< pair<int, float> > PatternGen<T>::getPattern(list< pair<int, float> > partes)
+{
+	// tratar la lista
+
+	// seleccionar patron (o algoritmo-que-hace-patrón) en función de la lista
+	Pattern* p = new SimplePattern();
+
+	//devolverlo
+	return p->createPattern(partes);
+}
+
+template <class T>
+list< pair< T, int> >* PatternGen<T>::getPattern(list< pair<T, int> >* partes)
+{
+	list< pair<int, float> > tmpin, tmpout;
+	list< pair<T, int> >* out = new list< pair<T, int> >();
+
+	int nsegmentos = 0;
+
+	for (list< pair<T, int> >::iterator it = partes->begin(); it != partes->end(); it++)
+	{
+		tmpin.push_back(make_pair((int) (*it).first,(*it).second));
+		nsegmentos += (*it).second;
+	}
+
+	tmpout = getPattern(tmpin);
+	
+	int tmpSum = 0;
+	int i = 0;
+	int tmpSeg;
+	for (list< pair<int, float> >::iterator it = tmpout.begin(); it != tmpout.end(); it++)
+	{
+		tmpSum += (*it).second;
+		if (i == tmpout.size() - 1) 
+			tmpSeg = nsegmentos - tmpSum + (*it).second;
+		else
+			tmpSeg = (*it).second;
+
+		out->push_back(make_pair((T) (*it).first,tmpSeg));
+		i++;
+	}
+
+	return out;
+}
 
 #endif // PATTERNGEN_H
