6cb7a10d8dc94a5da21c60710df26febc3d7d762
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2011-12-13 00:28:42 +0000

    + composerFigRitmo ya tiene incluída la funcionalidad que tenía composerritmo, por ahora vale. Me acuesto.

--- trunk/Muphic/Mu/source/include/ComposerFigRitmo.h
+++ trunk/Muphic/Mu/source/include/ComposerFigRitmo.h
@@ -4,17 +4,23 @@
 #define COMPOSERFIGRITMO_H
 
 
+#include "Figuras.h"
+#include "Segmento.h"
+#include "Nota.h"
+
 class ComposerFigRitmo
 {
     public:
 		ComposerFigRitmo() {};
 		virtual ~ComposerFigRitmo() {};
 
+		//Se pide que haga un ritmo dada una figura. Devuelve el segmento con el ritmo, de duracion dur
+		bool compMelodyFig(Figura* f, Segmento* seg, int dur);
 
 	protected:
 
 	private:
-
+		int getDrumTone(int duracion);
 };
 
 
