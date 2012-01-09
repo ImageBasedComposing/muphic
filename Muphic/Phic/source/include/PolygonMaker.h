71b6ead0f4f1f27a5b0c5401e5ef2a7690274517
strauzen@gmail.com <strauzen@gmail.com@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2012-01-09 17:07:08 +0000

    + Acabado PolygonMaker - No se puede saber si funciona porque RegionMaker le pasa 0 masks

--- Muphic/Phic/source/include/PolygonMaker.h
+++ Muphic/Phic/source/include/PolygonMaker.h
@@ -4,6 +4,7 @@
 #define POLYGONMAKER_H
 
 #include "aux_elements.h"
+#include "math_functions.h"
 
 using namespace std;
 
@@ -13,15 +14,18 @@ class PolygonMaker
 		PolygonMaker();
 		virtual ~PolygonMaker();
 
-		std::vector< Polygon >* makePolygons(std::vector< Mask >* masks);
-		void makePolygon(Mask m, FigureImg* f);
+		std::vector< Polygon > makePolygons(std::vector< Mask* > masks);
+		void makePolygon(Mask* m, FigureImg* f);
 
 	protected:
-		std::vector< Polygon >* polygon;
+		std::vector< Polygon > polygon;
 	
 	private:
-		bool isBorder(int i, int j, Mask m);
-		std::pair<int,int> checkDirection(int i, int j, Mask m, int* dir);
+		static const int VAR = 4;		//The lower this number the better the polygon aproximation, won't work with 0
+		static const int ERR = 22;	//The error margin between the two vectors of a polygon, the lower this number the more exact the polygon will be
+
+		bool isBorder(int i, int j, Mask* m);
+		std::pair<int,int> checkDirection(int i, int j, Mask* m, std::pair<int,int> initPos, int* dir, bool* isVertex);
 };
 
 #endif // POLYGONMAKER_H
--- Muphic/Phic/source/src/PolygonMaker.cpp
+++ Muphic/Phic/source/src/PolygonMaker.cpp
@@ -7,22 +7,21 @@ PolygonMaker::PolygonMaker()
 
 PolygonMaker::~PolygonMaker()
 {
-	polygon->clear();
-	delete polygon;
+	polygon.clear();
 }
 
 /*--- Main Functions ---*/
-std::vector< Polygon >* PolygonMaker::makePolygons(std::vector< Mask >* masks)
+std::vector< Polygon > PolygonMaker::makePolygons(std::vector< Mask* > masks)
 {
-	Mask mask;
+	Mask* mask;
 	FigureImg* f;
 	Polygon p;
 
 	// We take each element from the mask array and make its adequate Polygon
-	for(std::vector<Mask>::iterator it = masks->begin(); it != masks->end(); it++)
+	for(std::vector<Mask*>::iterator it = masks.begin(); it != masks.end(); it++)
 	{
 		// We adquire the next mask
-		mask = *it;
+		mask = (*it);
 
 		// We create a new figure where to add the colected data
 		f = new FigureImg();
@@ -31,28 +30,26 @@ std::vector< Polygon >* PolygonMaker::makePolygons(std::vector< Mask >* masks)
 		makePolygon(*it,f);
 
 		// We add the collected data to a Polygon type
-		p->first = *it;
-		p->second = f;
+		p.first = *it;
+		p.second = f;
 
 		// We add the data to the solution we will be returning
-		polygon->push_back(p);
+		polygon.push_back(p);
 	}
 
 	return polygon;
 }
 
-void PolygonMaker::makePolygon(Mask m, FigureImg* f)
+void PolygonMaker::makePolygon(Mask* m, FigureImg* f)
 {
 	bool found = false;
 	vector<bool>* aux;
 	std::pair<int,int> newVertex;
 	std::pair<int,int> startingPos;
+	bool* isVertex = false;
 	int* dir;
 	*dir = -361;
-
-	// Mirar si es 1, si es 1 mirar si esta rodeado de algun 0 para saber si es borde
-	// Si tiene borde comprobar con los 8 siguientes su direccion
-	// Si tiene 1 sola dir actualizar i y j, sino ir hasta el punto donde cambia la dir y actualizar i j
+	Vertice* v = new Vertice();
 
 	// We search for initial position of the polygon
 	for(int i = 0; !found && i < m->size(); i++)
@@ -65,7 +62,7 @@ void PolygonMaker::makePolygon(Mask m, FigureImg* f)
 			{
 				startingPos.first = i;
 				startingPos.second = j;
-				newVertex = checkDirection(i,j, m, dir);
+				newVertex = checkDirection(i,j, m, startingPos, dir, isVertex);
 				found = true;
 			}
 		}
@@ -74,18 +71,97 @@ void PolygonMaker::makePolygon(Mask m, FigureImg* f)
 	// We go through the contour of the polygon looking for its vertexes
 	while(newVertex != startingPos)
 	{
+		newVertex = checkDirection(newVertex.first, newVertex.second, m, startingPos, dir, isVertex);
+		if(*isVertex)
+		{
+			v = new Vertice();
+			v->x = newVertex.first;
+			v->y = newVertex.second;
+			f->colocarVertice(v);
+		}
 	}
 }
 
 /*--- Private Functions ---*/
-bool PolygonMaker::isBorder(int i, int j, Mask m)
+
+// This funcion checks if were in the border of the mask or if its the border of the figure
+bool PolygonMaker::isBorder(int i, int j, Mask* m)
 {
-	return (i == 0 || j == 0 || i == m->size() || j == m->at(i)->size() ); //falta si esta rodeado de algun 0
+	if (m->at(i)->at(j) == 1)
+	{
+		if (i == 0 || j == 0 || i == m->size() || j == m->at(i)->size())
+			return 1;
+		else
+			return (m->at(i)->at(j - 1) == 0	 || m->at(i)->at(j + 1) == 0	 ||
+					m->at(i - 1)->at(j) == 0	 || m->at(i + 1)->at(j) == 0	 ||
+					m->at(i - 1)->at(j - 1) == 0 || m->at(i - 1)->at(j + 1) == 0 ||
+					m->at(i + 1)->at(j - 1) == 0 || m->at(i + 1)->at(j + 1) == 0);
+	}
+	return 0;
 }
 
-std::pair<int,int> PolygonMaker::checkDirection(int i, int j, Mask m, int* dir)
+// Polygons will be made 4 by 4 bits, which means that when we find a corner it may
+// create a diagonal instead of drawing the corner correctly
+std::pair<int,int> PolygonMaker::checkDirection(int i, int j, Mask* m, std::pair<int,int> ip, int* dir, bool* isVertex)
 {
-	return std::make_pair(0,0);
+	std::pair<int,int> newVertex;
+	int *newDir;
+
+	*isVertex = false;
+
+	if(*dir != -361)
+	{
+		//We check the direction per quadrant to choose wich sorrounding pixels we're going to analyze
+		if((*dir >= 0 && *dir < 90) || *dir == 360)
+		{
+			for(int k = i; k >= 0 && k < i - VAR && k != ip.first; k--)
+				for(int t = j; t < m->at(i)->size() && t < j + VAR && t != ip.second; t++)
+					if(m->at(i)->at(j) == 1 && isBorder(i,j,m))
+						newVertex == std::make_pair(k,t);
+			*newDir = vectorAngle(i,j,newVertex.first,newVertex.second);
+		}
+		else if(*dir >= 90 && *dir < 180)
+		{
+			for(int k = i; k >= 0 && k < i - VAR && k != ip.first; k--)
+				for(int t = j; t >= 0 && t < j - VAR && t != ip.second; t--)
+					if(m->at(i)->at(j) == 1 && isBorder(i,j,m))
+						newVertex == std::make_pair(k,t);
+			*newDir = vectorAngle(i,j,newVertex.first,newVertex.second);
+		}
+		else if(*dir >= 180 && *dir < 270)
+		{
+			for(int k = i; k < m->size() && k < i + VAR && k != ip.first; k++)
+				for(int t = j; t >= 0 && t < j - VAR && t != ip.second; t--)
+					if(m->at(i)->at(j) == 1 && isBorder(i,j,m))
+						newVertex == std::make_pair(k,t);
+			*newDir = vectorAngle(i,j,newVertex.first,newVertex.second);
+		}
+		else if(*dir >= 270 && *dir < 360)
+		{
+			for(int k = i; k < m->size() && k < i + VAR && k != ip.first; k++)
+				for(int t = j; t < m->at(i)->size() && t < j + VAR && t != ip.second; j++)
+					if(m->at(i)->at(j) == 1 && isBorder(i,j,m))
+						newVertex == std::make_pair(k,t);
+			*newDir = vectorAngle(i,j,newVertex.first,newVertex.second);
+		}
+		if(fabs((float) *dir - *newDir) > ERR)
+		{
+			*dir = *newDir;
+			newVertex = std::make_pair(i,j);
+			*isVertex = true;
+		}
+	}
+	else
+	{
+		// We look down from the point and to the right because it's the first point we found
+		for(int k = i; k < m->size() && k < i + VAR; k++)
+			for(int t = j; j < m->at(i)->size() && t < j + VAR; j++)
+				if(m->at(i)->at(j) == 1 && isBorder(i,j,m))
+					newVertex == std::make_pair(k,t);
+		*dir = vectorAngle(i,j,newVertex.first,newVertex.second);
+	}
+
+	return newVertex;
 }
 
 /*--- Getters & Setters ---*/
