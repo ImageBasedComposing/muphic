faf3b1330d199cf35f72c3419fde3039c02084df
vlad12354 <vlad12354@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2012-02-29 01:32:20 +0000

    + Compilación en linux 32 bits.

--- GUI/GUIQt/moc_guimupic.cpp
+++ GUI/GUIQt/moc_guimupic.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'guimupic.h'
 **
-** Created: Mon Dec 19 23:48:16 2011
-**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
+** Created: Wed Feb 29 02:19:10 2012
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -11,7 +11,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'guimupic.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 62
-#error "This file was generated using the moc from 4.7.4. It"
+#error "This file was generated using the moc from 4.6.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,10 +20,10 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_GuiMupic[] = {
 
  // content:
-       5,       // revision
+       4,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+      10,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -36,6 +36,11 @@ static const uint qt_meta_data_GuiMupic[] = {
       78,    9,    9,    9, 0x08,
      111,    9,    9,    9, 0x08,
      140,    9,    9,    9, 0x08,
+     169,    9,    9,    9, 0x08,
+     207,  201,    9,    9, 0x08,
+     262,  253,    9,    9, 0x08,
+     301,  253,    9,    9, 0x08,
+     340,  253,    9,    9, 0x08,
 
        0        // eod
 };
@@ -46,6 +51,11 @@ static const char qt_meta_stringdata_GuiMupic[] = {
     "on_pushButton_Generate_clicked()\0"
     "on_pushButton_Stop_clicked()\0"
     "on_pushButton_Play_clicked()\0"
+    "on_pushButton_Analyze_clicked()\0index\0"
+    "on_filterSelComboBox_currentIndexChanged(int)\0"
+    "position\0on_horizontalSlider_2_sliderMoved(int)\0"
+    "on_horizontalSlider_3_sliderMoved(int)\0"
+    "on_horizontalSlider_6_sliderMoved(int)\0"
 };
 
 const QMetaObject GuiMupic::staticMetaObject = {
@@ -82,9 +92,14 @@ int GuiMupic::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         case 2: on_pushButton_Generate_clicked(); break;
         case 3: on_pushButton_Stop_clicked(); break;
         case 4: on_pushButton_Play_clicked(); break;
+        case 5: on_pushButton_Analyze_clicked(); break;
+        case 6: on_filterSelComboBox_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: on_horizontalSlider_2_sliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: on_horizontalSlider_3_sliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: on_horizontalSlider_6_sliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
-        _id -= 5;
+        _id -= 10;
     }
     return _id;
 }
