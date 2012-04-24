c3008835a417e9d6263495f54e2f2c240002f364
carloscatalanfdez <carloscatalanfdez@3067ad2c-94d2-e183-0483-fb873b2e9bae>
2012-04-24 00:33:44 +0000

    + cambiada la gui para que l amúsica que reproduzca sea la compuesta, pero no está actualizado el exe + quitado cosas que sobran de composervoice.cpp + actualizado proyecto de codeblocks

--- Muphic/Mu/source/src/Compositors/ComposerVoice.cpp
+++ Muphic/Mu/source/src/Compositors/ComposerVoice.cpp
@@ -1,14 +1,2 @@
 #include "Compositors/ComposerVoice.h"
 
-ComposerVoice::ComposerVoice(ColorSystem* cs)
-{
-	this->cs = cs;
-}
-
-ComposerVoice::~ComposerVoice()
-{
-    //dtor
-}
-
-
-string ComposerVoice::composeVoice() { return NULL; }
